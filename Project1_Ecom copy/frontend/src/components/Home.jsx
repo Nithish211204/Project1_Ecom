d28862741f3c44c9b1a99e49693d4e7a1112099f
Dhruvateja Amancha <123286539+Dhruva1129@users.noreply.github.com>
2025-01-10 19:15:41 +0530

    Update Home.jsx

--- Project1_Ecom copy/frontend/src/components/Home.jsx
+++ Project1_Ecom copy/frontend/src/components/Home.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import HomeCarousel from './HomeCarasoul';  
- // Import the CSS for Home page
+// import React from 'react';
+// import HomeCarousel from './HomeCarasoul';  
+//  // Import the CSS for Home page
 
-const Home = () => {
-//   const images = [
-//     'C:\Users\cheru\NITHISH\week-1\download (1).jpg',
-//     'C:\Users\cheru\NITHISH\week-1\download.jpg',
-//     'C:\Users\cheru\NITHISH\week-1\images (1).jpg',
-// ];
+// const Home = () => {
+// //   const images = [
+// //     'C:\Users\cheru\NITHISH\week-1\download (1).jpg',
+// //     'C:\Users\cheru\NITHISH\week-1\download.jpg',
+// //     'C:\Users\cheru\NITHISH\week-1\images (1).jpg',
+// // ];
 
-return (
-    <div>
-      <h1>Welcome to the Legumes Page</h1>
-      <HomeCarousel />
-    </div>
-);
-};
+// return (
+//     <div>
+//       <h1>Welcome to the Legumes Page</h1>
+//       <HomeCarousel />
+//     </div>
+// );
+// };
 
 
-export default Home;
+// export default Home;
